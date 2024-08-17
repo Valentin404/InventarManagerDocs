00f49859e314354939a078985f255bb4687a3b3f
Valentin404 <valik49717@gmail.com>
2024-08-17 13:24:09 +0300

    v0.0.005

--- script/lang.js
+++ script/lang.js
@@ -1,15 +1,198 @@
+
+
 const datas = {
     uk: {
         docs: [
             {
-                title: 'Як видалити аккаунт',
-                texts: [ ]
+                title: 'Як видалити акаунт',
+                texts: [ ],
+                type : 'account'
             },
             {
-                title: 'Як відплити співробітника',
-                texts: [ ]
+                title: 'Що робити якщо забув пароль',
+                texts: [ ],
+                type : 'account'
             },
-        ]
+            {
+                title: 'Як змінити пароль',
+                texts: [ ],
+                type : 'account'
+            },
+            {
+                title: 'Чи можна поміняти Email',
+                texts: [ ],
+                type : 'account'
+            },
+            {
+                title: 'Чому я не можу увійти в акаунт і мені потрібно чекати 24 години',
+                texts: [ ],
+                type : 'account'
+            },
+            {
+                title: 'Як змінити особисті дані',
+                texts: [ ],
+                type : 'account'
+            },
+            // --------------
+            {
+                title: 'Як перенести свою табличку excel у застосунок',
+                texts: [ ],
+                type : 'setting'
+            },
+            {
+                title: 'Як завантажити свої дані в додаток',
+                texts: [ ],
+                type : 'setting'
+            },
+            // --------------
+            {
+                title: 'Які дані збирає додаток',
+                texts: [ ],
+                type : 'confidentially'
+            },
+            {
+                title: 'Як запросити видалення даних',
+                texts: [ ],
+                type : 'confidentially'
+            },
+            {
+                title: 'Яку безпеку ви надаєте',
+                texts: [ ],
+                type : 'confidentially'
+            },
+            // --------------
+            {
+                title: 'Як залишити відгук про застосунок',
+                texts: [ ],
+                type : 'contact'
+            },
+            {
+                title: 'Як повідомити про баг або помилку',
+                texts: [ ],
+                type : 'contact'
+            },
+            // --------------
+            {
+                title: 'Як оформити підписку на преміум-послугиЯк підвищити статус підписки',
+                texts: [ ],
+                type : 'payment'
+            },
+            {
+                title: 'Які способи оплати підтримуються',
+                texts: [ ],
+                type : 'payment'
+            },
+            {
+                title: 'Які існують тарифні плани',
+                texts: [ ],
+                type : 'payment'
+            },
+            // --------------
+            {
+                title: 'Як видалити співробітника',
+                texts: [ ],
+                type : 'worker'
+            },
+            {
+                title: 'Як додати співробітника',
+                texts: [ ],
+                type : 'worker'
+            },
+            {
+                title: 'Як обмежити доступ співробітника',
+                texts: [ ],
+                type : 'worker'
+            },
+            // --------------
+            {
+                title: 'Що таке папка важливе',
+                texts: [ ],
+                type : 'folder'
+            },
+            {
+                title: 'Що таке папка обране',
+                texts: [],
+                type : 'folder'
+            },
+            // --------------
+            {
+                title: 'Як користуватися автозвітом',
+                texts: [ ],
+                type : 'transaction'
+            },
+            {
+                title: 'Що таке надходження',
+                texts: [ ],
+                type : 'transaction'
+            },
+            {
+                title: 'Що таке продаж',
+                texts: [ ],
+                type : 'transaction'
+            },
+            {
+                title: 'Що таке повернення',
+                texts: [ ],
+                type : 'transaction'
+            },
+            // --------------
+            {
+                title: 'Що таке документи',
+                texts: [],
+                type : 'docum'
+            },
+            // --------------
+            {
+                title: 'Я користувач Inventory Manager. З чого почати',
+                texts: [ ],
+                type : 'function'
+            },
+            {
+                title: 'Які функції доступні в додатку',
+                texts: [ ],
+                type : 'function'
+            },
+            {
+                title: 'Чи підтримується сканування штрихкодів та qr-кодів? Як це працює',
+                texts: [ ],
+                type : 'function'
+            },
+            {
+                title: 'Що таке офлайн режим',
+                texts: [ ],
+                type : 'function'
+            },
+            {
+                title: 'Як здійснюється відстеження запасів ',
+                texts: [ ],
+                type : 'function'
+            },
+            {
+                title: 'Як синхронізувати дані з іншими пристроями',
+                texts: [ ],
+                type : 'function'
+            },
+            // --------------
+            {
+                title: 'Що означає ця кнопка (img)',
+                texts: [ ],
+                img : '',
+                type : 'button'
+            },     
+        ],
+        types : {
+            account : 'Акаунт',
+            setting : 'Налаштування та персоналізація',
+            confidentially : 'Конфиденциальность и данные',
+            contact : 'Контакти та підтримка',
+            payment : 'Оплата та підписка',
+            worker : 'Співробітники',
+            folder : 'Папки',
+            transaction : 'Управління транзакціями',
+            docum : 'Документи',
+            function : 'Функції та можливості',
+            button : 'Кнопки'
+        }
     },
     eng: {
         docs: [
@@ -26,4 +209,34 @@ const datas = {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+
+
+
 const lang = datas['uk']['docs'];
+
+
+
+
+
+
