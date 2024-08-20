61238a3034f447b9515f9573bf58104d6bdfa62f
Vladimir Sychevsky <reactorswins@ukr.net>
2024-08-20 21:04:43 +0300

    re

--- script/docs.js
+++ script/docs.js
@@ -3,7 +3,6 @@ console.log(lang);
 const questionsSection = document.querySelector(".questions");
 
 lang.forEach((q) => {
-  let visible = false;
   const question = document.createElement("a");
   question.classList.add("question");
   question.textContent = q.title;
@@ -16,7 +15,7 @@ const filter = () => {
   const filterText = searchedInput.value.toLowerCase();
 
   questions.forEach((q) => {
-    const qText = q.textContent;
+    const qText = q.textContent.toLocaleLowerCase();
     if (qText.includes(filterText)) {
       let highLightText = qText.replace(filterText, `<span class="highLight">${filterText}</span>`);
       q.innerHTML = highLightText;
