95bed32c52416c91b815ad388eda1a7f38a72464
Valentin404 <valik49717@gmail.com>
2024-10-04 12:56:53 +0300

    v0.0.015

---
+++ script/current.faq.js
@@ -0,0 +1,17 @@
+// http://127.0.0.1:5500/docs.example.html?lang=uk&qwestion=1
+// Получаем текущий URL
+
+const urlParams = new URLSearchParams(window.location.search);
+const langURL = urlParams.get('lang');
+const questionURL = urlParams.get('qwestion');
+
+// const getQwestion = () => {
+//     try {
+//         return lang[langURL]?.docs[questionURL];
+//     } catch (e){
+//         const d = lang.eng.docs[questionURL];
+//         if(d) return d;
+//         return lang.eng.docs[0] 
+//     }
+// }
+
---
+++ script/datas.js
@@ -0,0 +1,266 @@
+
+
+
+const datas = {
+    docs: [
+        {
+            title: 'Як видалити акаунт',
+            answer: [
+                {
+                    teg : 'img',
+                    atributes : {src : './images/faq/1/1.jpg'}
+                }
+            ],
+            type: 'account',
+            _id : '1'
+
+        },
+        {
+            title: 'Що робити якщо забув пароль',
+            answer: [],
+            type: 'account',
+            _id : '2'
+        },
+        {
+            title: 'Як змінити пароль',
+            answer: [],
+            type: 'account',
+            _id : '3'
+        },
+        {
+            title: 'Чи можна поміняти Email',
+            answer: [],
+            type: 'account',
+            _id : '4'
+        },
+        {
+            title: 'Чому я не можу увійти в акаунт і мені потрібно чекати 24 години',
+            answer: [],
+            type: 'account',
+            _id : '5'
+        },
+        {
+            title: 'Як змінити особисті дані',
+            answer: [],
+            type: 'account',
+            _id : '6'
+        },
+        // --------------
+        {
+            title: 'Як перенести свою табличку excel у застосунок',
+            answer: [],
+            type: 'setting',
+            _id : '7'
+        },
+        {
+            title: 'Як завантажити свої дані в додаток',
+            answer: [],
+            type: 'setting',
+            _id : '8'
+        },
+        // --------------
+        {
+            title: 'Які дані збирає додаток',
+            answer: [],
+            type: 'confidentially',
+            _id : '9'
+        },
+        {
+            title: 'Як запросити видалення даних',
+            answer: [],
+            type: 'confidentially',
+            _id : '10'
+        },
+        {
+            title: 'Яку безпеку ви надаєте',
+            answer: [],
+            type: 'confidentially',
+            _id : '11'
+        },
+        // --------------
+        {
+            title: 'Як залишити відгук про застосунок',
+            answer: [],
+            type: 'contact',
+            _id : '12'
+        },
+        {
+            title: 'Як повідомити про баг або помилку',
+            answer: [],
+            type: 'contact',
+            _id : '13'
+        },
+        // --------------
+        {
+            title: 'Як оформити підписку на преміум-послугиЯк підвищити статус підписки',
+            answer: [],
+            type: 'payment',
+            _id : '14'
+        },
+        {
+            title: 'Які способи оплати підтримуються',
+            answer: [],
+            type: 'payment',
+            _id : '15'
+        },
+        {
+            title: 'Які існують тарифні плани',
+            answer: [],
+            type: 'payment',
+            _id : '16'
+        },
+        // --------------
+        {
+            title: 'Як видалити співробітника',
+            answer: [],
+            type: 'worker',
+            _id : '17'
+        },
+        {
+            title: 'Як додати співробітника',
+            answer: [],
+            type: 'worker',
+            _id : '18'
+        },
+        {
+            title: 'Як обмежити доступ співробітника',
+            answer: [],
+            type: 'worker',
+            _id : '19'
+        },
+        // --------------
+        {
+            title: 'Що таке папка важливе',
+            answer: [],
+            type: 'folder',
+            _id : '20'
+        },
+        {
+            title: 'Що таке папка обране',
+            answer: [],
+            type: 'folder',
+            _id : '21'
+        },
+        // --------------
+        {
+            title: 'Як користуватися автозвітом',
+            answer: [],
+            type: 'transaction',
+            _id : '22'
+        },
+        {
+            title: 'Що таке надходження',
+            answer: [],
+            type: 'transaction',
+            _id : '23'
+        },
+        {
+            title: 'Що таке продаж',
+            answer: [],
+            type: 'transaction',
+            _id : '24'
+        },
+        {
+            title: 'Що таке повернення',
+            answer: [],
+            type: 'transaction',
+            _id : '25'
+        },
+        // --------------
+        {
+            title: 'Що таке документи',
+            answer: [],
+            type: 'docum',
+            _id : '26'
+        },
+        // --------------
+        {
+            title: 'Я користувач Inventory Manager. З чого почати',
+            answer: [],
+            type: 'function',
+            _id : '27'
+        },
+        {
+            title: 'Які функції доступні в додатку',
+            answer: [],
+            type: 'function',
+            _id : '28'
+        },
+        {
+            title: 'Чи підтримується сканування штрихкодів та qr-кодів? Як це працює',
+            answer: [],
+            type: 'function',
+            _id : '29'
+        },
+        {
+            title: 'Що таке офлайн режим',
+            answer: [],
+            type: 'function',
+            _id : '30'
+        },
+        {
+            title: 'Як здійснюється відстеження запасів ',
+            answer: [],
+            type: 'function',
+            _id : '31'
+        },
+        {
+            title: 'Як синхронізувати дані з іншими пристроями',
+            answer: [],
+            type: 'function',
+            _id : '32'
+        },
+        // --------------
+        {
+            title: 'Що означає ця кнопка (img)',
+            answer: [],
+            img: '',
+            type: 'button',
+            _id : '33'
+        },
+    ],
+    types: {
+        account: 'Акаунт',
+        setting: 'Налаштування та персоналізація',
+        confidentially: 'Конфиденциальность и данные',
+        contact: 'Контакти та підтримка',
+        payment: 'Оплата та підписка',
+        worker: 'Співробітники',
+        folder: 'Папки',
+        transaction: 'Управління транзакціями',
+        docum: 'Документи',
+        function: 'Функції та можливості',
+        button: 'Кнопки'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const lang = datas['uk']['docs'];
+
+
+
+
+
