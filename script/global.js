169675f9c6079e178f3b43b8975b39b34b9f4754
Valentin404 <valik49717@gmail.com>
2024-10-18 20:59:44 +0300

    v0.0.046

--- script/global.js
+++ script/global.js
@@ -1,10 +1,16 @@
 const headerMobileBtn = document.querySelector('#header-mobile-btn');
 const navMenuMobile = document.querySelector('.nav-menu-mobile');
 const html = document.querySelector('html');
+const contFilterBlack = document.querySelector('.cont-filter-black');
 
 headerMobileBtn.addEventListener('click', ()=> {
     headerMobileBtn.classList.toggle('menu-open')
     navMenuMobile.classList.toggle('menu-open')
     html.classList.toggle('lock-scroll');
-    // document.querySelector('.links').classList.toggle('mobileMenu')
+})
+
+contFilterBlack.addEventListener('click', ()=> {
+    headerMobileBtn.classList.toggle('menu-open')
+    navMenuMobile.classList.toggle('menu-open')
+    html.classList.toggle('lock-scroll');
 })
