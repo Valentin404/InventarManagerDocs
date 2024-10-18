e8b7fe26686177875b68023fb24d698807481384
Valentin404 <valik49717@gmail.com>
2024-10-18 20:16:48 +0300

    v0.0.043

--- script/global.js
+++ script/global.js
@@ -4,6 +4,6 @@ const navMenuMobile = document.querySelector('.nav-menu-mobile');
 headerMobileBtn.addEventListener('click', ()=> {
     headerMobileBtn.classList.toggle('menu-open')
     navMenuMobile.classList.toggle('menu-open')
-    // document.body.classList.toggle('lock-scroll');
+    document.body.classList.toggle('lock-scroll');
     // document.querySelector('.links').classList.toggle('mobileMenu')
 })
