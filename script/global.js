c03cf3b66f739233c222e2985d7b13e36d786166
Vladimir Sychevsky <reactorswins@ukr.net>
2024-10-18 03:50:36 +0300

    hamburger

--- script/global.js
+++ script/global.js
@@ -1,6 +1,12 @@
 const headerMobileBtn = document.querySelector('#header-mobile-btn');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
 
 headerMobileBtn.addEventListener('click', ()=> {
     headerMobileBtn.classList.toggle('menu-open')
     // document.querySelector('.links').classList.toggle('mobileMenu')
+    
+    //Блокирую скролл
+    document.body.classList.toggle('lock-scroll');
+    //Добавляю клас active для hamburger-menu
+    hamburgerMenu.classList.toggle('active');
 })
