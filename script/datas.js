2263113f41720a84d7058c4917a1da8f39405917
Valentin404 <valik49717@gmail.com>
2024-10-21 19:17:12 +0300

    v0.0.047

--- script/datas.js
+++ script/datas.js
@@ -1,10 +1,10 @@
 
 
-
+ 
 const datas = {
     docs: [
         {
-            title: 'Як видалити акаунт',
+            title: 'How to delete an account',
             answer: [
                 {   
                     conent : [
@@ -49,207 +49,398 @@ const datas = {
 
         },
         {
-            title: 'Що робити якщо забув пароль',
-            answer: [],
+            title: 'What to do if I forgot my password',
+            answer: [
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/2/1.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/2/2.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                },
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/2/3.jpg'},
+                            className : "photo-mid"
+                        }
+                    ],
+                    className : 'cont',
+                    classNameCont : 'center' 
+                }
+            ],
             type: 'account',
             _id : '2'
         },
         {
-            title: 'Як змінити пароль',
-            answer: [],
+            title: 'How to change the password',
+            answer: [
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/3/1.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/3/2.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                },
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/3/3.jpg'},
+                            className : "photo-mid"
+                        }
+                    ],
+                    className : 'cont',
+                    classNameCont : 'center' 
+                }
+            ],
             type: 'account',
             _id : '3'
         },
         {
-            title: 'Чи можна поміняти Email',
-            answer: [],
+            title: 'Can I change my email',
+            answer: [
+                  {   
+                    conent : [
+                        {
+                            teg : 'p',
+                            className : "text",
+                            text : "At the moment, it is not possible to change the account email. This feature may become available in future updates."
+                        }
+                    ],
+                    className : 'cont',
+                    classNameCont : 'center mb25' 
+                }
+            ],
             type: 'account',
             _id : '4'
         },
         {
-            title: 'Чому я не можу увійти в акаунт і мені потрібно чекати 24 години',
-            answer: [],
+            title: 'Why can\'t I log into my account and why do I have to wait 24 hours',
+            answer: [
+                {   
+                    conent : [
+                        {
+                            teg : 'p',
+                            className : "text",
+                            text : "For user security, one account can only be logged into on one device at a time, and the device can be changed 24 hours after logging in. This ensures that no one but you can access your account from another device. In the future, if absolutely necessary, you can contact technical support, and we can lift this restriction."
+                        }
+                    ],
+                    className : 'cont',
+                    classNameCont : 'center mb25' 
+                }
+            ],
             type: 'account',
             _id : '5'
         },
         {
-            title: 'Як змінити особисті дані',
-            answer: [],
+            title: 'How to update personal information',
+            answer: [
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/6/1.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/6/2.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                },
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/6/3.jpg'},
+                            className : "photo-mid"
+                        }
+                    ],
+                    className : 'cont',
+                    classNameCont : 'center' 
+                }
+            ],
             type: 'account',
             _id : '6'
         },
         // --------------
-        {
+        {   
             title: 'Як перенести свою табличку excel у застосунок',
             answer: [],
             type: 'setting',
-            _id : '7'
+            _id : '' //!
         },
         {
-            title: 'Як завантажити свої дані в додаток',
-            answer: [],
+            title: 'How to upload your data into the app',
+            answer: [
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/7/1.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/7/2.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                },
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/7/3.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/7/4.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                },
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/7/5.jpg'},
+                            className : "photo-mid"
+                        }
+                    ],
+                    className : 'cont',
+                    classNameCont : 'center' 
+                }
+            ],
             type: 'setting',
-            _id : '8'
+            _id : '7'
         },
         // --------------
         {
             title: 'Які дані збирає додаток',
             answer: [],
             type: 'confidentially',
-            _id : '9'
+            _id : '' //!
         },
         {
-            title: 'Як запросити видалення даних',
+            title: 'How to request data deletion',
             answer: [],
             type: 'confidentially',
+            _id : '8'
+        },
+        {
+            title: 'What security measures do you provide',
+            answer: [],
+            type: 'confidentially',
+            _id : '9'
+        },
+        // --------------
+        {
+            title: 'How to leave feedback about the app',
+            answer: [],
+            type: 'contact',
             _id : '10'
         },
         {
-            title: 'Яку безпеку ви надаєте',
+            title: 'How to report a bug or error',
             answer: [],
-            type: 'confidentially',
+            type: 'contact',
             _id : '11'
         },
         // --------------
         {
-            title: 'Як залишити відгук про застосунок',
+            title: 'How to subscribe to premium services / How to upgrade your subscription',
             answer: [],
-            type: 'contact',
+            type: 'payment',
             _id : '12'
         },
         {
-            title: 'Як повідомити про баг або помилку',
+            title: 'What payment methods are supported',
             answer: [],
-            type: 'contact',
+            type: 'payment',
             _id : '13'
         },
-        // --------------
         {
-            title: 'Як оформити підписку на преміум-послугиЯк підвищити статус підписки',
+            title: 'What subscription plans are available',
             answer: [],
             type: 'payment',
             _id : '14'
         },
+        // --------------
         {
-            title: 'Які способи оплати підтримуються',
-            answer: [],
-            type: 'payment',
+            title: 'How to remove an employee',
+            answer: [
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/15/1.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/15/2.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                },
+                {   
+                    conent : [
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/15/3.jpg'},
+                            className : "photo-left"
+                        },
+                        {
+                            teg : 'img',
+                            atributes : {src : './images/faq/15/4.jpg'},
+                            className : "photo-left"
+                        },
+                    ],
+                    className : 'cont cont-2' 
+                }
+            ],
+            type: 'worker',
             _id : '15'
         },
         {
-            title: 'Які існують тарифні плани',
+            title: 'How to add an employee',
             answer: [],
-            type: 'payment',
+            type: 'worker',
             _id : '16'
         },
-        // --------------
         {
-            title: 'Як видалити співробітника',
+            title: 'How to restrict employee access',
             answer: [],
             type: 'worker',
             _id : '17'
         },
+        // --------------
         {
-            title: 'Як додати співробітника',
+            title: 'What is the "Important" folder',
             answer: [],
-            type: 'worker',
+            type: 'folder',
             _id : '18'
         },
         {
-            title: 'Як обмежити доступ співробітника',
+            title: 'What is the "Favorites" folder',
             answer: [],
-            type: 'worker',
+            type: 'folder',
             _id : '19'
         },
         // --------------
         {
-            title: 'Що таке папка важливе',
+            title: 'How to use the Auto Report feature',
             answer: [],
-            type: 'folder',
+            type: 'transaction',
             _id : '20'
         },
         {
-            title: 'Що таке папка обране',
+            title: 'What is Receiving',
             answer: [],
-            type: 'folder',
+            type: 'transaction',
             _id : '21'
         },
-        // --------------
         {
-            title: 'Як користуватися автозвітом',
+            title: 'What is Sales',
             answer: [],
             type: 'transaction',
             _id : '22'
         },
         {
-            title: 'Що таке надходження',
+            title: 'What is Returns',
             answer: [],
             type: 'transaction',
-            _id : '23'
-        },
-        {
-            title: 'Що таке продаж',
-            answer: [],
-            type: 'transaction',
-            _id : '24'
-        },
-        {
-            title: 'Що таке повернення',
-            answer: [],
-            type: 'transaction',
-            _id : '25'
+            _id : '23' 
         },
         // --------------
         {
             title: 'Що таке документи',
             answer: [],
             type: 'docum',
-            _id : '26'
+            _id : '' //!
         },
         // --------------
         {
-            title: 'Я користувач Inventory Manager. З чого почати',
+            title: 'I am an Inventory Manager user. Where do I start',
+            answer: [],
+            type: 'function',
+            _id : '24'
+        },
+        {
+            title: 'What features are available in the app',
+            answer: [],
+            type: 'function',
+            _id : '25'
+        },
+        {
+            title: 'Does the app support barcode and QR code scanning? How does it work',
+            answer: [],
+            type: 'function',
+            _id : '26'
+        },
+        {
+            title: 'What is offline mode',
             answer: [],
             type: 'function',
             _id : '27'
         },
         {
-            title: 'Які функції доступні в додатку',
+            title: 'How is stock tracking performed',
             answer: [],
             type: 'function',
             _id : '28'
         },
         {
-            title: 'Чи підтримується сканування штрихкодів та qr-кодів? Як це працює',
+            title: 'How to synchronize data with other devices',
             answer: [],
             type: 'function',
             _id : '29'
         },
         {
-            title: 'Що таке офлайн режим',
+            title: 'What is a barcode',
             answer: [],
             type: 'function',
             _id : '30'
         },
-        {
-            title: 'Як здійснюється відстеження запасів ',
-            answer: [],
-            type: 'function',
-            _id : '31'
-        },
-        {
-            title: 'Як синхронізувати дані з іншими пристроями',
-            answer: [],
-            type: 'function',
-            _id : '32'
-        },
         // --------------
         {
-            title: 'Що означає ця кнопка (img)',
+            title: 'What does this button do (img)',
             answer: [],
             img: '',
             type: 'button',
-            _id : '33'
+            _id : '31' 
+        },
+        {
+            title: 'What does this button do (img)',
+            answer: [],
+            img: '',
+            type: 'button',
+            _id : '32' 
         },
     ],
     types: {
