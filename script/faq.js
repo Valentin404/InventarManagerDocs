d96d61de7a0ffcfd3083158bac69b99d935dc84d
Vladimir Sychevsky <reactorswins@ukr.net>
2024-12-17 17:49:51 +0200

    mobile FAQ

--- script/faq.js
+++ script/faq.js
@@ -1,111 +1,112 @@
-const panelToggle = document.querySelector('.show-hide-panel');
+(function () {
+  const types = datas.types;
 
-(function(){
-const types = datas.types;
+  const questionsCategory = document.querySelector("#questionsCategory");
+  const answersStack = document.querySelector("#answersStack");
+  const questionTitle = document.querySelector("#questionTitle");
+  const questionTitle2 = document.querySelector("#questionTitle2");
+  const searchInput = document.querySelector("#searchInput");
+  const searchInput2 = document.querySelector("#searchInput2");
+  const rightPanel = document.querySelector(".right-panel");
+  const panelToggle = document.querySelector(".show-hide-panel");
 
-const questionsCategory = document.querySelector('#questionsCategory')
-const answersStack = document.querySelector('#answersStack')
-const questionTitle = document.querySelector('#questionTitle')
-const searchInput = document.querySelector('#searchInput')
+  function removeLastActive() {
+    const lastActive = document.querySelector(".text.active-press");
+    lastActive?.classList.remove("active-press");
+  }
 
+  function searchedQuestions(el) {
+    const container = document.createElement("div");
+    const readMoreBtn = document.createElement("a");
+    const ask = document.createElement("p");
 
-function removeLastActive() {
-  const lastActive = document.querySelector('.text.active-press')
-  lastActive?.classList.remove('active-press')
-}
+    container.classList.add("ask-card");
+    container.classList.add("center-column");
 
-function searchedQuestions(el) {
-  const container = document.createElement("div");
-  const readMoreBtn = document.createElement("a");
-  const ask = document.createElement("p");
+    readMoreBtn.classList.add("btn");
+    ask.classList.add("center");
+    ask.classList.add("text");
 
-  container.classList.add("ask-card");
-  container.classList.add("center-column");
+    readMoreBtn.textContent = "Read more";
 
-  readMoreBtn.classList.add("btn");
-  ask.classList.add('center');
-  ask.classList.add("text");
-
-  readMoreBtn.textContent = "Read more";
-
-  readMoreBtn.href = location.hostname.includes('127')
-    ? location.origin + '/FAQcurrent.html?qwestion=' + el._id
-    : 'https://valentin404.github.io/InventarManagerDocs/FAQcurrent.html?qwestion=' + el._id // for reliz
+    readMoreBtn.href = location.hostname.includes("127")
+      ? location.origin + "/FAQcurrent.html?qwestion=" + el._id
+      : "https://valentin404.github.io/InventarManagerDocs/FAQcurrent.html?qwestion=" +
+        el._id; // for reliz
 
     container.append(ask);
-    
-    if(el.img) {
+
+    if (el.img) {
       const img = document.createElement("img");
       img.src = el.img;
-      img.classList.add('img-text-qwestion')
+      img.classList.add("img-text-qwestion");
       container.append(img);
-    } 
-    
-    ask.textContent = el.title + ' ?';
+    }
 
-  container.append(readMoreBtn);
-  answersStack.appendChild(container);
-}
+    ask.textContent = el.title + " ?";
 
-Object.keys(types).forEach((type) => {
-  const question = document.createElement("span");
-  question.textContent = types[type];
-  question.classList.add("text");
-  questionsCategory.appendChild(question);
-  question.slot = type;
-
-
-  question.onclick = () => {
-    removeLastActive()
-    question.classList.add("active-press");
-    answersStack.innerHTML = "";
-    questionTitle.textContent = question.textContent;
-
-    datas.docs
-      .filter((el) => el.type === type)
-      .forEach(searchedQuestions);
-  };
-
-
-});
-
-// questionsCategory.addEventListener("click", (e) => {
-//   const slot = e.target.slot;
-//   if(!slot) return;
-//   const isClickQuestion = e.target.closest(".text");
-//   const isClickBtn = e.target.closest(".btn");
-//   console.log(isClickQuestion)
-//   console.log(isClickBtn, 'isClickBtn')
-//   if (!isClickQuestion && !isClickBtn) {
-//     document
-//       .querySelectorAll("span")
-//       .forEach((el) => el.classList.remove("active-press"));
-//     answersStack.innerHTML = "";
-//     questionTitle.textContent = "Оберіть категорію питання";
-//   }
-// });
-
-searchInput.addEventListener("input", () => {
-  answersStack.innerHTML = "";
-  if (searchInput.value.trim()) {
-    removeLastActive()
-    datas.docs
-      .filter((el) => {
-        const regex = new RegExp(searchInput.value, "gi");
-        return el.title.match(regex);
-      })
-      .forEach(searchedQuestions);
-
-    questionTitle.textContent = 'Текст для ручного поиска';
+    container.append(readMoreBtn);
+    answersStack.appendChild(container);
   }
-});
 
-console.log('-----------faq 0.0.001 ------')
+  Object.keys(types).forEach((type) => {
+    const question = document.createElement("span");
+    question.textContent = types[type];
+    question.classList.add("text");
+    questionsCategory.appendChild(question);
+    question.slot = type;
 
+    question.onclick = () => {
+      removeLastActive();
+      searchInput.value = searchInput2.value = "";
+      question.classList.add("active-press");
+      answersStack.innerHTML = "";
+      questionTitle.textContent = questionTitle2.textContent = question.textContent;
+
+      datas.docs.filter((el) => el.type === type).forEach(searchedQuestions);
+    };
+  });
+
+  // questionsCategory.addEventListener("click", (e) => {
+  //   const slot = e.target.slot;
+  //   if(!slot) return;
+  //   const isClickQuestion = e.target.closest(".text");
+  //   const isClickBtn = e.target.closest(".btn");
+  //   console.log(isClickQuestion)
+  //   console.log(isClickBtn, 'isClickBtn')
+  //   if (!isClickQuestion && !isClickBtn) {
+  //     document
+  //       .querySelectorAll("span")
+  //       .forEach((el) => el.classList.remove("active-press"));
+  //     answersStack.innerHTML = "";
+  //     questionTitle.textContent = "Оберіть категорію питання";
+  //   }
+  // });
+
+  function searchOptions() {
+    answersStack.innerHTML = "";
+    searchInput.value = this.value;
+    searchInput2.value = this.value;
+    if (this.value.trim()) {
+      removeLastActive();
+      datas.docs
+        .filter((el) => {
+          const regex = new RegExp(this.value, "gi");
+          return el.title.match(regex);
+        })
+        .forEach(searchedQuestions);
+
+      questionTitle.textContent = questionTitle2.textContent = "Text for manual entry"
+    }
+  }
+  searchInput.addEventListener("input", searchOptions);
+  searchInput2.addEventListener("input", searchOptions);
+
+  panelToggle.addEventListener("click", () => {
+    rightPanel.classList.toggle("show-panel");
+    panelToggle.classList.toggle("show-panel");
+    html.classList.toggle("lock-scroll");
+  });
+
+  console.log("-----------faq 0.0.001 ------");
 })();
-
-panelToggle.addEventListener('click', () => {
-  questionsCategory.classList.toggle('show-panel');
-  panelToggle.classList.toggle('show-panel');
-  html.classList.toggle('lock-scroll');
-});
